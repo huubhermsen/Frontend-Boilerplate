f8663a8bf99ea65154252a506bb0d988a6c85b2a
Huub Hermsen <huub@osage.nl>
2014-12-17 12:14:02 +0100

    Removed busy support due to bad support in libsass

--- source/tasks/sass.js
+++ source/tasks/sass.js
@@ -1,6 +1,6 @@
 module.exports = {
     options: {
-        includePaths: ["./source/bower/susy/sass/", "./source/bower/normalize-scss/"]
+        includePaths: ["<%= config.path.source %>/bower/normalize-scss/"]
     },
     dist: {
         files: { "<%= config.path.test %>/<%= config.asset.css %>/default.css": "<%= config.path.source %>/sass/default.scss" }
